ddaae0d2512a4169f65115fd6c62f0baf350b7cd
simsax <simsax98@gmail.com>
2023-05-18 18:58:11 +0200

    chunks of bytecode

--- part_II/src/debug.c
+++ part_II/src/debug.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "debug.h"
+#include "value.h"
 
 void disassemble_chunk(Chunk* chunk, const char* name)
 {
@@ -17,12 +18,29 @@ static int simple_instruction(const char* name, int offset)
     return offset + 1;
 }
 
+static int constant_instruction(const char* name, Chunk* chunk, int offset)
+{
+    uint8_t constant_index = chunk->code[offset + 1];
+    printf("%-16s %4d '", name, constant_index);
+    print_value(chunk->constants.values[constant_index]);
+    printf("'\n");
+    // OP_CONSTANT is 2 bytes (opcode, operand)
+    return offset + 2;
+}
+
 int disassemble_instruction(Chunk* chunk, int offset)
 {
     printf("%04d ", offset);
+    if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
+        printf("   | ");
+    } else {
+        printf("%4d ", chunk->lines[offset]);
+    }
 
     uint8_t instruction = chunk->code[offset];
     switch (instruction) {
+    case OP_CONSTANT:
+        return constant_instruction("OP_CONSTANT", chunk, offset);
     case OP_RETURN:
         return simple_instruction("OP_RETURN", offset);
     default:
--- part_II/src/value.h
+++ part_II/src/value.h
@@ -5,4 +5,15 @@
 
 typedef double Value;
 
+typedef struct {
+    int capacity;
+    int count;
+    Value* values;
+} ValueArray;
+
+void init_value_array(ValueArray* array);
+void write_value_array(ValueArray* array, Value value);
+void free_value_array(ValueArray* array);
+void print_value(Value value);
+
 #endif
